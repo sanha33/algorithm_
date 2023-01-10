418bf025e379f22f97c13bd17a2a50515ab723a6
sanha33 <48049882+sanha33@users.noreply.github.com>
2023-01-10 17:11:10 +0900

    3. 조건문(if 분기문, 다중 if문)
    1/10(화)

---
+++ 조건문 if.py
@@ -0,0 +1,48 @@
+'''
+조건문 if(분기, 중첩)
+'''
+
+x = 5
+if x!=7:
+    print("Lucky") # 들여쓰기 4칸 기본 
+    print("ㅋㅋ")
+
+
+y = 15
+if y>=10:
+    if x%2==1:
+        print("10이상의 홀수")
+
+
+z = 7
+if z>0 and z<10:
+    print("10보다 작은 자연수")
+
+
+a = 3
+if 0<a<10:
+    print("10보다 작은 자연수")
+
+
+
+# 분기 
+b = 10
+if b > 0 :
+    print("양수")
+else:
+    print("음수")
+
+
+c = 93
+if c>=90:
+    print('A')
+elif c>=80:
+    print('B')
+elif c>=70:
+    print('C')
+elif c>=60:
+    print('D')
+else:
+    print('F')
+    
+
